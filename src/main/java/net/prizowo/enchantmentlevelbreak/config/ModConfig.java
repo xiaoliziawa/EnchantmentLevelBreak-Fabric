2ca67511eb7113a4cc38121d1096dba05e443686
xiaoliziawa <3682822069@qq.com>
2024-12-26 16:18:32 +0800

    add config

---
+++ src/main/java/net/prizowo/enchantmentlevelbreak/config/ModConfig.java
@@ -0,0 +1,70 @@
+package net.prizowo.enchantmentlevelbreak.config;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class ModConfig {
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    private static final Path CONFIG_PATH = FabricLoader.getInstance().getConfigDir().resolve("enchantmentlevelbreak.json");
+    private static ModConfig INSTANCE;
+
+    private boolean useRomanNumerals = true;
+    private int romanNumeralsThreshold = 5000;
+
+    public static ModConfig getInstance() {
+        if (INSTANCE == null) {
+            load();
+        }
+        return INSTANCE;
+    }
+
+    public boolean isUseRomanNumerals() {
+        return useRomanNumerals;
+    }
+
+    public int getRomanNumeralsThreshold() {
+        return romanNumeralsThreshold;
+    }
+
+    public void setUseRomanNumerals(boolean useRomanNumerals) {
+        this.useRomanNumerals = useRomanNumerals;
+        save();
+    }
+
+    public void setRomanNumeralsThreshold(int threshold) {
+        this.romanNumeralsThreshold = threshold;
+        save();
+    }
+
+    public static void load() {
+        try {
+            if (Files.exists(CONFIG_PATH)) {
+                try (Reader reader = Files.newBufferedReader(CONFIG_PATH)) {
+                    INSTANCE = GSON.fromJson(reader, ModConfig.class);
+                }
+            } else {
+                INSTANCE = new ModConfig();
+                INSTANCE.save();
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to load config: " + e.getMessage());
+            INSTANCE = new ModConfig();
+        }
+    }
+
+    private void save() {
+        try {
+            Files.createDirectories(CONFIG_PATH.getParent());
+            try (Writer writer = Files.newBufferedWriter(CONFIG_PATH)) {
+                GSON.toJson(this, writer);
+            }
+        } catch (IOException e) {
+            System.err.println("Failed to save config: " + e.getMessage());
+        }
+    }
+}
