0ded27cbf56631cd3121b75e7021ed3bb5ff8c70
xiaoliziawa <3682822069@qq.com>
2024-12-26 16:17:55 +0800

    add config

--- src/main/java/net/prizowo/enchantmentlevelbreak/CEnchantCommand.java
+++ src/main/java/net/prizowo/enchantmentlevelbreak/CEnchantCommand.java
@@ -11,7 +11,6 @@ import net.minecraft.registry.DynamicRegistryManager;
 import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.resource.ResourceType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
